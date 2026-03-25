fb1138e5870f0af30b7a62bba1c988ececa66bb3
takuro <takuro.tokunaga@utah.edu>
2026-03-24 20:57:13 -0700

    upload my local to dev_1

--- main.cpp
+++ main.cpp
@@ -3,22 +3,9 @@
 
 int main()
 {
-    Message obj_m;    
-    FEM obj_fe;
-    Boundary obj_bc;
-    Solver obj_solver;
-    Display obj_disp;
-    File obj_file;
-
+    Message obj_m;        
     obj_m.start();
-    FEM::FeedToSolver tensors = obj_fe.matrix(); /* define struct */
-
-    obj_bc.boundary_d(&tensors.Amat, &tensors.bmat);
-    obj_bc.boundary_n(&tensors.Amat, &tensors.bmat);    
-    /* obj_disp.show(&tensors.Amat, &tensors.bmat); */
-
-    obj_solver.pivlu(&tensors.Amat, &tensors.bmat, &tensors.uvector);
-    obj_file.writer(&tensors.xvector, &tensors.uvector);
+    
 
     obj_m.end();    
 
